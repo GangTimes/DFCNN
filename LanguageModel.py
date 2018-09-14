8c0389c1ca39391159746b1ec7444f50ab6942f2
代祥 <daixiang@daixiang.local>
2018-09-14 14:49:37 +0800

    update

--- LanguageModel.py
+++ LanguageModel.py
@@ -62,78 +62,48 @@ class ModelLanguage(): # 语音模型类
 			r += str_decode[0][0]
 		return r
 	
-	def decode(self,list_syllable, yuzhi = 0.0001):
+	def decode(self, list_syllable, yuzhi = 0.0001):
 		'''
 		实现拼音向文本的转换
 		基于马尔可夫链
 		'''
-		#assert self.dic_pinyin == null or self.model1 == null or self.model2 == null
 		list_words = []
-		
-		num_pinyin = len(list_syllable)
-		#print('======')
-		#print('decode function: list_syllable\n',list_syllable)
-		#print(num_pinyin)
-		# 开始语音解码
-		for i in range(num_pinyin):
-			#print(i)
+		for i in range(len(list_syllable)):
 			ls = ''
 			if(list_syllable[i] in self.dict_pinyin): # 如果这个拼音在汉语拼音字典里的话
 				# 获取拼音下属的字的列表，ls包含了该拼音对应的所有的字
 				ls = self.dict_pinyin[list_syllable[i]]
 			else:
 				break
-			
-			
-			if(i == 0):
+			if i == 0:
 				# 第一个字做初始处理
-				num_ls = len(ls)
-				for j in range(num_ls):
-					tuple_word = ['',0.0]
-					# 设置马尔科夫模型初始状态值
-					# 设置初始概率，置为1.0
-					tuple_word = [ls[j], 1.0]
-					#print(tuple_word)
-					# 添加到可能的句子列表
-					list_words.append(tuple_word)
-				
-				#print(list_words)
+				# 设置马尔科夫模型初始状态值
+				# 设置初始概率，置为1.0
+				list_words = [[ls[j], 1.0] for j in range(len(ls))]
 				continue
 			else:
 				# 开始处理紧跟在第一个字后面的字
 				list_words_2 = []
-				num_ls_word = len(list_words)
-				#print('ls_wd: ',list_words)
-				for j in range(0, num_ls_word):
-					
-					num_ls = len(ls)
-					for k in range(0, num_ls):
-						tuple_word = ['',0.0]
+				print('ls_wd: ',list_words)
+				for j in range(len(list_words)):
+					for k in range(len(ls)):
 						tuple_word = list(list_words[j]) # 把现有的每一条短语取出来
-						#print('tw1: ',tuple_word)
 						tuple_word[0] = tuple_word[0] + ls[k] # 尝试按照下一个音可能对应的全部的字进行组合
-						#print('ls[k]  ',ls[k])
-						
+						print('ls[k]  ',tuple_word)
 						tmp_words = tuple_word[0][-2:] # 取出用于计算的最后两个字
 						#print('tmp_words: ',tmp_words,tmp_words in self.model2)
-						if(tmp_words in self.model2): # 判断它们是不是再状态转移表里
-							#print(tmp_words,tmp_words in self.model2)
+						if tmp_words in self.model2: # 判断它们是不是再状态转移表里
+							# 核心！在当前概率上乘转移概率，公式简化为第n-1和n个字出现的次数除以第n-1个字出现的次数
 							tuple_word[1] = tuple_word[1] * float(self.model2[tmp_words]) / float(self.model1[tmp_words[-2]])
-							# 核心！在当前概率上乘转移概率，公式化简后为第n-1和n个字出现的次数除以第n-1个字出现的次数
-							#print(self.model2[tmp_words],self.model1[tmp_words[-2]])
 						else:
-							tuple_word[1] = 0.0
 							continue
-						#print('tw2: ',tuple_word)
-						#print(tuple_word[1] >= pow(yuzhi, i))
 						if(tuple_word[1] >= pow(yuzhi, i)):
 							# 大于阈值之后保留，否则丢弃
 							list_words_2.append(tuple_word)
-						
 				list_words = list_words_2
 				#print(list_words,'\n')
 		#print(list_words)
-		for i in range(0, len(list_words)):
+		for i in range(len(list_words)):
 			for j in range(i + 1, len(list_words)):
 				if(list_words[i][1] < list_words[j][1]):
 					tmp = list_words[i]
@@ -211,7 +181,7 @@ if __name__=='__main__':
 	
 	#str_pinyin = ['zhe4','zhen1','shi4','ji2', 'hao3','de5']
 	#str_pinyin = ['jin1', 'tian1', 'shi4', 'xing1', 'qi1', 'san1']
-	#str_pinyin = ['ni3', 'hao3','a1']
+	str_pinyin = ['ni3', 'hao3','a1']
 	#str_pinyin = ['wo3','dui4','shi4','mei2','cuo4','ni3','hao3']
 	#str_pinyin = ['wo3','dui4','shi4','tian1','mei2','na5','li3','hai4']
 	#str_pinyin = ['ba3','zhe4','xie1','zuo4','wan2','wo3','jiu4','qu4','shui4','jiao4']
@@ -219,7 +189,7 @@ if __name__=='__main__':
 	#str_pinyin = ['wo3', 'men5', 'qun2', 'li3', 'xiong1', 'di4', 'jian4', 'mei4', 'dou1', 'zai4', 'shuo1']
 	#str_pinyin = ['su1', 'an1', 'ni3', 'sui4', 'li4', 'yun4', 'sui2', 'cong2', 'jiao4', 'ming2', 'tao2', 'qi3', 'yu2', 'peng2', 'ya4', 'yang4', 'chao1', 'dao3', 'jiang1', 'li3', 'yuan2', 'kang1', 'zhua1', 'zou3']
 	#str_pinyin = ['da4', 'jia1', 'hao3']
-	str_pinyin = ['kao3', 'yan2', 'yan1', 'yu3', 'ci2', 'hui4']
+	#str_pinyin = ['kao3', 'yan2', 'yu3', 'ci2', 'hui4']
 	#r = ml.decode(str_pinyin)
 	r=ml.SpeechToText(str_pinyin)
 	print('语音转文字结果：\n',r)
